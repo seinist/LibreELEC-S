3c350fe84bded31079a7a03c3b434a12f8cc7bf9
SupervisedThinking <supervisedthinking@gmail.com>
2021-03-29 21:39:09 +0200

    kodi: add custom menu icons

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -256,6 +256,16 @@ configure_package() {
                          ${KODI_PIPEWIRE}"
 }
 
+post_unpack(){
+  if [ ! "$OEM_APPS" = "no" ] && [ "$PROJECT" = "Generic" ]; then
+    cp $PKG_DIR/files/app-menu-icons/* $PKG_BUILD/addons/skin.estuary/media/
+  fi
+
+  if [ ! "$OEM_EMU" = "no" ]; then
+    cp $PKG_DIR/files/emu-menu-icons/* $PKG_BUILD/addons/skin.estuary/media/
+  fi
+}
+
 pre_configure_target() {
   export LIBS="${LIBS} -lncurses"
 }
