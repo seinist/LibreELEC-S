ed1e33d81405300b4c106a0599e359b7725aef6e
SupervisedThinking <supervisedthinking@gmail.com>
2021-10-26 20:01:39 +0200

    packages/supervisedthinking: added various "emulation/libretro" packages

---
+++ packages/supervisedthinking/emulation/libretro/kronos/package.mk
@@ -0,0 +1,62 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="kronos"
+PKG_VERSION="f2359c25f3fc7180be1ad890ba477ad415957ef6"
+PKG_SHA256="300074fab0f1549fe28851ba34f5cdc6078fcc2abce0d04e94d056634b17bd88"
+PKG_LICENSE="GPL-2.0-or-later"
+PKG_SITE="https://github.com/libretro/yabause"
+PKG_URL="https://github.com/libretro/yabause/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux glibc"
+PKG_LONGDESC="Kronos is a Sega Saturn emulator forked from uoYabause. "
+PKG_TOOLCHAIN="make"
+PKG_BUILD_FLAGS="+lto -sysroot"
+
+PKG_LIBNAME="kronos_libretro.so"
+PKG_LIBPATH="yabause/src/libretro/${PKG_LIBNAME}"
+
+PKG_MAKE_OPTS_TARGET="-C yabause/src/libretro GIT_VERSION=${PKG_VERSION:0:7}"
+
+configure_package() {
+  # Displayserver Support
+  if [ "${DISPLAYSERVER}" = "x11" ]; then
+    PKG_DEPENDS_TARGET+=" xorg-server"
+  fi
+
+  # OpenGL Support
+  if [ "${OPENGL_SUPPORT}" = "yes" ]; then
+    PKG_DEPENDS_TARGET+=" ${OPENGL}"
+  fi
+
+  # OpenGLES Support
+  if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
+    PKG_DEPENDS_TARGET+=" ${OPENGLES}"
+    PKG_PATCH_DIRS="OpenGLES"
+  fi
+}
+
+pre_configure_target() {
+  if [ "${ARCH}" = "arm" ]; then
+    if [ "${DEVICE}" = "RK3399" ]; then
+      PKG_MAKE_OPTS_TARGET+=" platform=rockpro64"
+    elif [ "${DEVICE}" = "AMLG12" ]; then
+      PKG_MAKE_OPTS_TARGET+=" platform=amlg12"
+    else
+      PKG_MAKE_OPTS_TARGET+=" platform=armv"
+      # ARM NEON support
+      if target_has_feature neon; then
+        PKG_MAKE_OPTS_TARGET+="-neon"
+      fi
+      PKG_MAKE_OPTS_TARGET+="-${TARGET_FLOAT}float-${TARGET_CPU}"
+    fi
+  fi
+}
+
+pre_make_target() {
+  make CC=${HOST_CC} -C yabause/src/libretro generate-files
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+}
---
+++ packages/supervisedthinking/emulation/libretro/mame2010/package.mk
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="mame2010"
+PKG_VERSION="932e6f2c4f13b67b29ab33428a4037dee9a236a8"
+PKG_SHA256="af60ec225fc4470e7bbc4a2e35cc7d4e1498aed4fe35b8f238fd5ba0dd9ead26"
+PKG_LICENSE="GPL-2.0-or-later"
+PKG_SITE="https://github.com/libretro/mame2010-libretro"
+PKG_URL="https://github.com/libretro/mame2010-libretro/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux glibc zlib"
+PKG_LONGDESC="Late 2010 version of MAME (0.139) for libretro. Compatible with MAME 0.139 romsets."
+PKG_TOOLCHAIN="make"
+PKG_BUILD_FLAGS="-sysroot"
+
+PKG_LIBNAME="mame2010_libretro.so"
+PKG_LIBPATH="${PKG_LIBNAME}"
+
+PKG_MAKE_OPTS_TARGET="GIT_VERSION=${PKG_VERSION:0:7}"
+
+pre_configure_target() {
+  if [ "${ARCH}" = "arm" ]; then
+    PKG_MAKE_OPTS_TARGET+=" platform=armv"
+    # ARM NEON support
+    if target_has_feature neon; then
+      PKG_MAKE_OPTS_TARGET+="-neon"
+    fi
+    PKG_MAKE_OPTS_TARGET+="-${TARGET_FLOAT}float-${TARGET_CPU}"
+  fi
+  # Fix linking
+  export LD="${CXX}"
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp -v ${PKG_LIBPATH} ${INSTALL}/usr/lib/libretro/
+}
