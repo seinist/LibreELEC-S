7609ffc4a39fbf0da57b0e34adab71e29303f4f4
SupervisedThinking <supervisedthinking@gmail.com>
2021-10-26 20:04:24 +0200

    packages/supervisedthinking: added various "multimedia" packages

---
+++ packages/supervisedthinking/multimedia/SDL/sdl_gamecontrollerdb/package.mk
@@ -0,0 +1,32 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="sdl_gamecontrollerdb"
+PKG_VERSION="f06d4da6cff3fd044c4f003e2ea8807a51903b74"
+PKG_SHA256="fb7035487b700b46a88eecfd47f0d239edb308a5be34327b97eb01d114dd89a3"
+PKG_LICENSE="SDL"
+PKG_SITE="https://github.com/gabomdq/SDL_GameControllerDB"
+PKG_URL="https://github.com/gabomdq/SDL_GameControllerDB/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET=""
+PKG_LONGDESC="A community sourced database of game controller mappings to be used with SDL2 Game Controller functionality"
+PKG_TOOLCHAIN="manual"
+
+makeinstall_target() {
+  # Paths
+  PKG_CONFIG_PATH="/usr/config/SDL-GameControllerDB"
+  PKG_SHARE_PATH="/usr/share/sdl-gamecontrollerdb"
+  PKG_DATABASE_PATH="${PKG_BUILD}/gamecontrollerdb.txt"
+
+  # Clean up database
+  sed -e "/Windows/d" -i ${PKG_DATABASE_PATH}
+  sed -e "/Mac/d"     -i ${PKG_DATABASE_PATH}
+  sed -e "/Android/d" -i ${PKG_DATABASE_PATH}
+  sed -e "/iOS/d"     -i ${PKG_DATABASE_PATH}
+  sed -e "/^$/d"      -i ${PKG_DATABASE_PATH}
+
+  # Install files
+  mkdir -p ${INSTALL}/${PKG_CONFIG_PATH}
+  mkdir -p ${INSTALL}/${PKG_SHARE_PATH}
+   cp -v ${PKG_DATABASE_PATH} ${INSTALL}/${PKG_SHARE_PATH}
+   ln -sf ${PKG_SHARE_PATH}/gamecontrollerdb.txt ${INSTALL}/${PKG_CONFIG_PATH}/gamecontrollerdb.txt
+}
