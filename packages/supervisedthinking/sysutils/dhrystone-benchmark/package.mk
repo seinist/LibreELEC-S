0873188f0c7d244c024cb8d765bbe4d12658ff40
SupervisedThinking <supervisedthinking@gmail.com>
2021-10-26 20:06:20 +0200

    packages/supervisedthinking: added various "sysutils" packages

---
+++ packages/supervisedthinking/sysutils/dhrystone-benchmark/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2019-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="dhrystone-benchmark"
+PKG_VERSION="d015cf2ee4417f6bc383b0fcbfd7a940ed32e2e9" #v2.1
+PKG_SHA256="b92e935aa7a25dba9d8b822175230035090c56837e3bc086d97d374ec38322a1"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/qris/dhrystone-deb"
+PKG_URL="https://github.com/qris/dhrystone-deb/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Dhrystone Benchmark"
