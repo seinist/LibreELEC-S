954c25fde184c048666afd229a2ab5a48fa3e67e
SupervisedThinking <supervisedthinking@gmail.com>
2021-10-26 20:06:20 +0200

    packages/supervisedthinking: added various "sysutils" packages

---
+++ packages/supervisedthinking/sysutils/skyscraper/package.mk
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="skyscraper"
+PKG_VERSION="3.6.15"
+PKG_SHA256="de09316f3c1ad5c4591941f25ad307e65f66a328e7f848d8e9b32e776d1e6292"
+PKG_LICENSE="GPL-3.0-or-later"
+PKG_SITE="https://github.com/muldjord/skyscraper"
+PKG_URL="https://github.com/muldjord/skyscraper/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain qt-everywhere"
+PKG_LONGDESC="A powerful and versatile yet easy to use game scraper written in C++ for use with multiple frontends running on a Linux system."
+PKG_TOOLCHAIN="make"
+
+configure_target() {
+  # Fix install paths
+  sed -e "s#target.path=/usr/local/bin#target.path=${INSTALL}/usr/bin#"                                                       -i ${PKG_BUILD}/skyscraper.pro
+  sed -e "s#examples.path=/usr/local/etc/skyscraper#examples.path=${INSTALL}/usr/share/skyscraper#"                           -i ${PKG_BUILD}/skyscraper.pro
+  sed -e "s#cacheexamples.path=/usr/local/etc/skyscraper/cache#cacheexamples.path=${INSTALL}/usr/share/skyscraper/cache#"     -i ${PKG_BUILD}/skyscraper.pro
+  sed -e "s#impexamples.path=/usr/local/etc/skyscraper/import#impexamples.path=${INSTALL}/usr/share/skyscraper/import#"       -i ${PKG_BUILD}/skyscraper.pro
+  sed -e "s#resexamples.path=/usr/local/etc/skyscraper/resources#resexamples.path=${INSTALL}/usr/share/skyscraper/resources#" -i ${PKG_BUILD}/skyscraper.pro
+
+  # Generate qmake config
+  safe_remove .qmake.stash
+  qmake ${PKG_BUILD}/skyscraper.pro
+}
+
+post_makeinstall_target() {
+  # Install scripts 
+  mv ${INSTALL}/usr/bin/Skyscraper ${INSTALL}/usr/bin/skyscraper-bin
+  ln -sf /usr/bin/skyscraper       ${INSTALL}/usr/bin/Skyscraper
+  cp ${PKG_DIR}/scripts/*          ${INSTALL}/usr/bin/
+
+  # Install config
+  mkdir -p ${INSTALL}/usr/config/skyscraper
+  cp ${PKG_DIR}/config/* ${INSTALL}/usr/config/skyscraper/
+}
