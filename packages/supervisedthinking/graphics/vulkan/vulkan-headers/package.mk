ccbc92db0c7cad11f7f4d95d8deab5c6ce987fc3
SupervisedThinking <supervisedthinking@gmail.com>
2021-10-26 20:03:01 +0200

    packages/supervisedthinking: added various "graphics" packages

---
+++ packages/supervisedthinking/graphics/vulkan/vulkan-headers/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="vulkan-headers"
+PKG_VERSION="1.2.196"
+PKG_SHA256="33cb99194b5ab082beb00bda1e96311dfe2cb20b0037b6d4c8ae926a50f5a750"
+PKG_LICENSE="Apache 2.0"
+PKG_SITE="https://github.com/KhronosGroup/Vulkan-Headers"
+PKG_URL="https://github.com/KhronosGroup/Vulkan-Headers/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain cmake:host"
+PKG_LONGDESC="Vulkan Header files and API registry"
