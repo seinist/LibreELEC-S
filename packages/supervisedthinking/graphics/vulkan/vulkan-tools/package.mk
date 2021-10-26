3ac10b25595b961fedc9b836d8fdb7815ae2e894
SupervisedThinking <supervisedthinking@gmail.com>
2021-10-26 20:03:01 +0200

    packages/supervisedthinking: added various "graphics" packages

---
+++ packages/supervisedthinking/graphics/vulkan/vulkan-tools/package.mk
@@ -0,0 +1,26 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="vulkan-tools"
+PKG_VERSION="1.2.196"
+PKG_SHA256="378d774070f736ea0d750860824ae0bf3e663ed8839123115120761163d871f7"
+PKG_LICENSE="Apache 2.0"
+PKG_SITE="https://github.com/KhronosGroup/Vulkan-Tools"
+PKG_URL="https://github.com/KhronosGroup/Vulkan-tools/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain cmake:host vulkan-loader"
+PKG_LONGDESC="This project provides Khronos official Vulkan Tools and Utilities."
+
+pre_configure_target() {
+  PKG_CMAKE_OPTS_TARGET="-DBUILD_ICD=Off \
+                         -DINSTALL_ICD=Off"
+
+  # Disable Wayland WSI support
+  sed -e "s/Build Wayland WSI support\" ON/Build Wayland WSI support\" OFF/" -i ${PKG_BUILD}/cube/CMakeLists.txt
+  sed -e "s/Build Wayland WSI support\" ON/Build Wayland WSI support\" OFF/" -i ${PKG_BUILD}/vulkaninfo/CMakeLists.txt
+}
+
+pre_make_target() {
+  # Fix cross compiling
+  find ${PKG_BUILD} -name flags.make -exec sed -i  "s:isystem :I:g" \{} \;
+  find ${PKG_BUILD} -name build.ninja -exec sed -i "s:isystem :I:g" \{} \;
+}
