095188516214a318b15ca7ce6c49dfd15fb9fdab
SupervisedThinking <supervisedthinking@gmail.com>
2021-10-26 20:01:15 +0200

    packages/supervisedthinking: added various "emulation/frontends" packages

---
+++ packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/retroarch-assets/package.mk
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="retroarch-assets"
+PKG_VERSION="62b5d18b8141113c55d6db768882dc25b30ace45"
+PKG_SHA256="c86b37384767265c11e855580888c8c792b41b24136a7e8c05c0092aacec394a"
+PKG_LICENSE="CC-BY-4.0"
+PKG_SITE="https://github.com/libretro/retroarch-assets"
+PKG_URL="https://github.com/libretro/retroarch-assets/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="gcc:host"
+PKG_LONGDESC="RetroArch assets. Background and icon themes for the menu drivers."
+PKG_TOOLCHAIN="manual"
+
+makeinstall_target() {
+  cd ${PKG_BUILD}
+  make install INSTALLDIR="${INSTALL}/usr/share/retroarch/assets"
+}
