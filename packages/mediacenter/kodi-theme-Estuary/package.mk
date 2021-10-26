5c1cac141b443fae97da2ede453041d39baabf26
SupervisedThinking <supervisedthinking@gmail.com>
2021-10-26 19:36:00 +0200

    kodi-theme-Estuary: add menu patches

--- packages/mediacenter/kodi-theme-Estuary/package.mk
+++ packages/mediacenter/kodi-theme-Estuary/package.mk
@@ -14,4 +14,34 @@ PKG_TOOLCHAIN="manual"
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/share/kodi/addons/
     cp -a $(get_install_dir kodi)/.noinstall/skin.estuary ${INSTALL}/usr/share/kodi/addons/
+
+  # Add Brave & Spotify shortcuts to menu
+  if [ ! "${OEM_APPLICATIONS}" = "no" ] && [ "$PROJECT" = "Generic" ]; then
+    echo "### Adding Brave & Spotify to Estuary menu ###"
+    patch -d ${INSTALL}/usr/share/kodi/addons/skin.estuary -p1 < ${PKG_DIR}/files/kodi-theme-Estuary-100.01-application-menu.patch
+  fi
+
+  # Add Moonlight shortcut to menu 
+  if [ ! "${OEM_STREAMING_CLIENTS}" = "no" ]; then
+    echo "### Adding Moonlight-Qt to Estuary menu ###"
+    patch -d ${INSTALL}/usr/share/kodi/addons/skin.estuary -p1 < ${PKG_DIR}/files/kodi-theme-Estuary-100.05-moonlight-qt-menu.patch
+  fi
+
+  # Add Emulationstation shortcut to menu 
+  if [ ! "${OEM_EMULATORS}" = "no" ]; then
+    echo "### Adding Emulationstation to Estuary menu ###"
+    patch -d ${INSTALL}/usr/share/kodi/addons/skin.estuary -p1 < ${PKG_DIR}/files/kodi-theme-Estuary-100.02-emulationstation-menu.patch
+  fi
+
+  # Add Retroarch shortcut to menu 
+  if [ ! "${OEM_LIBRETRO}" = "no" ]; then
+    echo "### Adding Retroarch to Estuary menu ###"
+    patch -d ${INSTALL}/usr/share/kodi/addons/skin.estuary -p1 < ${PKG_DIR}/files/kodi-theme-Estuary-100.03-retroarch-menu.patch
+  fi
+
+  # Add Pegasus Frontend shortcut to menu 
+  if [ ! "${OEM_FRONTENDS_EXTRA}" = "no" ]; then
+    echo "### Adding Pegasus-Frontend to Estuary menu ###"
+    patch -d ${INSTALL}/usr/share/kodi/addons/skin.estuary -p1 < ${PKG_DIR}/files/kodi-theme-Estuary-100.04-pegasus-menu.patch
+  fi
 }
