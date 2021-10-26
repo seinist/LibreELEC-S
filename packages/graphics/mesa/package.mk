cdee260d9d9813d2573436c56ccdaa91920f4eca
SupervisedThinking <supervisedthinking@gmail.com>
2021-10-26 19:54:52 +0200

    mesa: enable zstd mesa cache compression

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -8,7 +8,7 @@ PKG_SHA256="0971226b4a6a3d10cfc255736b33e4017e18c14c9db1e53863ac1f8ae0deb9ea"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://mesa.freedesktop.org/archive/mesa-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="toolchain expat libdrm Mako:host"
+PKG_DEPENDS_TARGET="toolchain expat libdrm Mako:host zstd"
 PKG_LONGDESC="Mesa is a 3-D graphics library with an API."
 PKG_TOOLCHAIN="meson"
 
