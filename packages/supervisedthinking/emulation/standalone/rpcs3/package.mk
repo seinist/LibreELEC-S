5a605513f2fe10c7b725f37223873abc50497d6b
SupervisedThinking <supervisedthinking@gmail.com>
2021-11-03 16:54:44 +0100

    rpcs3: fix passing CMake cflags / dropped lto as binary segfaults
    - https://github.com/LibreELEC/LibreELEC.tv/issues/5486#issuecomment-878324296

--- packages/supervisedthinking/emulation/standalone/rpcs3/package.mk
+++ packages/supervisedthinking/emulation/standalone/rpcs3/package.mk
@@ -11,7 +11,6 @@ PKG_DEPENDS_HOST="toolchain:host"
 PKG_DEPENDS_TARGET="toolchain linux glibc systemd pulseaudio mesa xorg-server openal-soft-system libevdev curl ffmpeg libpng zlib vulkan-loader glew-cmake libSM sdl2 enet-system qt-everywhere unclutter-xfixes rpcs3:host"
 PKG_LONGDESC="RPCS3 is an experimental open-source Sony PlayStation 3 emulator and debugger."
 GET_HANDLER_SUPPORT="git"
-PKG_BUILD_FLAGS="+lto"
 
 pre_configure_host() {
   PKG_CMAKE_SCRIPT="${PKG_BUILD}/llvm/CMakeLists.txt"
@@ -24,11 +23,12 @@ pre_configure_host() {
                        -D LLVM_INCLUDE_TESTS=OFF \
                        -D LLVM_INCLUDE_TOOLS=OFF \
                        -D LLVM_INCLUDE_UTILS=OFF \
-                       -D LLVM_CCACHE_BUILD=ON"
+                       -D LLVM_CCACHE_BUILD=ON \
+                       -Wno-dev"
 }
 
 pre_configure_target() {
-  PKG_CMAKE_OPTS_TARGET="-D USE_NATIVE_INSTRUCTIONS=OFF \
+  PKG_CMAKE_OPTS_TARGET=(-D USE_NATIVE_INSTRUCTIONS=OFF \
                          -D BUILD_LLVM_SUBMODULE=ON \
                          -D CMAKE_C_FLAGS="${CFLAGS}" \
                          -D CMAKE_CXX_FLAGS="${CXXFLAGS}" \
@@ -39,7 +39,13 @@ pre_configure_target() {
                          -D USE_SYSTEM_FFMPEG=ON \
                          -D USE_SYSTEM_LIBPNG=ON \
                          -D USE_SYSTEM_ZLIB=ON \
-                         -D USE_SYSTEM_CURL=ON"
+                         -D USE_SYSTEM_CURL=ON \
+                         -Wno-dev)
+}
+
+configure_target() {
+  echo "Executing (target): cmake ${CMAKE_GENERATOR_NINJA} ${TARGET_CMAKE_OPTS} "${PKG_CMAKE_OPTS_TARGET[@]}" $(dirname ${PKG_CMAKE_SCRIPT})" | tr -s " "
+  cmake ${CMAKE_GENERATOR_NINJA} ${TARGET_CMAKE_OPTS} "${PKG_CMAKE_OPTS_TARGET[@]}" $(dirname ${PKG_CMAKE_SCRIPT})
 }
 
 make_host() {
