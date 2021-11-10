4f24331dc48532dac09adf38c9683cf57849661d
SupervisedThinking <supervisedthinking@gmail.com>
2021-11-10 13:18:53 +0100

    vulkan: update to 1.2.198

--- packages/supervisedthinking/graphics/vulkan/vulkan-tools/package.mk
+++ packages/supervisedthinking/graphics/vulkan/vulkan-tools/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="vulkan-tools"
-PKG_VERSION="1.2.196"
-PKG_SHA256="378d774070f736ea0d750860824ae0bf3e663ed8839123115120761163d871f7"
+PKG_VERSION="1.2.198"
+PKG_SHA256="22f602c0ebc922f3238fa5967e9a4c64ff7fe526434fd015fa5e427ab8684e3f"
 PKG_LICENSE="Apache 2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Tools"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-tools/archive/v${PKG_VERSION}.tar.gz"
