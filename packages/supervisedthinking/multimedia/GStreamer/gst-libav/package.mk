efdbb01b207329056291f75b3495f4f8e22e256a
SupervisedThinking <supervisedthinking@gmail.com>
2021-11-11 21:27:08 +0100

    GStreamer: update to 1.18.5

--- packages/supervisedthinking/multimedia/GStreamer/gst-libav/package.mk
+++ packages/supervisedthinking/multimedia/GStreamer/gst-libav/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="gst-libav"
-PKG_VERSION="1.18.4"
-PKG_SHA256="344a463badca216c2cef6ee36f9510c190862bdee48dc4591c0a430df7e8c396"
+PKG_VERSION="1.18.5"
+PKG_SHA256="822e008a910e9dd13aedbdd8dc63fedef4040c0ee2e927bab3112e9de693a548"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://gstreamer.freedesktop.org/modules/gst-libav.html"
 PKG_URL="https://gstreamer.freedesktop.org/src/gst-libav/${PKG_NAME}-${PKG_VERSION}.tar.xz"
