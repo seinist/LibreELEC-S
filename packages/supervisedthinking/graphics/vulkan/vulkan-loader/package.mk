89cfe04b1a865869400c7e62ba0e7380d2df9713
SupervisedThinking <supervisedthinking@gmail.com>
2021-11-16 18:06:58 +0100

    vulkan-loader: updated CMake opts for Wayland - fixed package download

--- packages/supervisedthinking/graphics/vulkan/vulkan-loader/package.mk
+++ packages/supervisedthinking/graphics/vulkan/vulkan-loader/package.mk
@@ -2,13 +2,13 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="vulkan-loader"
-PKG_VERSION="0d34e69625bb06d367d18e7c9f792ff34ba8b592" #v1.2.198
+PKG_VERSION="1.2.198"
+PKG_SHA256="b4a0ec6ce8921f50ab291f1c3a16802cfbe2c3dbd86820eb699bfb98890ad126"
 PKG_LICENSE="Apache 2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Loader"
-PKG_URL="https://github.com/KhronosGroup/Vulkan-Loader.git"
+PKG_URL="https://github.com/KhronosGroup/Vulkan-Loader/archive/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain cmake:host vulkan-headers"
 PKG_LONGDESC="Vulkan Installable Client Driver (ICD) Loader."
-GET_HANDLER_SUPPORT="git"
 
 configure_package() {
   # Displayserver Support
@@ -19,12 +19,16 @@ configure_package() {
 
 pre_configure_target() {
   PKG_CMAKE_OPTS_TARGET="-DBUILD_TESTS=Off \
-                         -DGIT_FOUND=Off \
-                         -DBUILD_WSI_WAYLAND_SUPPORT=Off"
+                         -DGIT_FOUND=Off"
 
-  # Mali Vulkan support
-  if [ ! "${DISPLAYSERVER}" = "x11" ]; then
+  if [ "${DISPLAYSERVER}" = "x11" ]; then
+    PKG_CMAKE_OPTS_TARGET+=" -DBUILD_WSI_WAYLAND_SUPPORT=Off"
+  elif [ "${DISPLAYSERVER}" = "weston" ]; then
     PKG_CMAKE_OPTS_TARGET+=" -DBUILD_WSI_XCB_SUPPORT=Off \
+                             -DBUILD_WSI_XLIB_SUPPORT=Off"
+  else
+    PKG_CMAKE_OPTS_TARGET+=" -DBUILD_WSI_WAYLAND_SUPPORT=Off \
+                             -DBUILD_WSI_XCB_SUPPORT=Off \
                              -DBUILD_WSI_XLIB_SUPPORT=Off"
   fi
 }
