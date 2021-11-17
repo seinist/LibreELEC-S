b85f35b7b4ea620b02f9d752ff88ed178010900d
SupervisedThinking <supervisedthinking@gmail.com>
2021-11-17 13:22:37 +0100

    vulkan-loader: updated to 1.2.199

--- packages/supervisedthinking/graphics/vulkan/vulkan-loader/package.mk
+++ packages/supervisedthinking/graphics/vulkan/vulkan-loader/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="vulkan-loader"
-PKG_VERSION="1.2.198"
-PKG_SHA256="b4a0ec6ce8921f50ab291f1c3a16802cfbe2c3dbd86820eb699bfb98890ad126"
+PKG_VERSION="1.2.199"
+PKG_SHA256="cc496f6725c7e088510d1a5e7c6a97b61e356b147dcc3d697233ca775dd768ef"
 PKG_LICENSE="Apache 2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Loader"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-Loader/archive/v${PKG_VERSION}.tar.gz"
@@ -27,8 +27,8 @@ pre_configure_target() {
     PKG_CMAKE_OPTS_TARGET+=" -DBUILD_WSI_XCB_SUPPORT=Off \
                              -DBUILD_WSI_XLIB_SUPPORT=Off"
   else
-    PKG_CMAKE_OPTS_TARGET+=" -DBUILD_WSI_WAYLAND_SUPPORT=Off \
-                             -DBUILD_WSI_XCB_SUPPORT=Off \
-                             -DBUILD_WSI_XLIB_SUPPORT=Off"
+    PKG_CMAKE_OPTS_TARGET+=" -DBUILD_WSI_XCB_SUPPORT=Off \
+                             -DBUILD_WSI_XLIB_SUPPORT=Off \
+                             -DBUILD_WSI_WAYLAND_SUPPORT=Off"
   fi
 }
