a810858a43be4e1b8076bb8ccd7b4b06328447d9
SupervisedThinking <supervisedthinking@gmail.com>
2021-11-17 13:22:57 +0100

    vulkan-tools: updated to 1.2.199 / fixed build

--- packages/supervisedthinking/graphics/vulkan/vulkan-tools/package.mk
+++ packages/supervisedthinking/graphics/vulkan/vulkan-tools/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="vulkan-tools"
-PKG_VERSION="1.2.198"
-PKG_SHA256="22f602c0ebc922f3238fa5967e9a4c64ff7fe526434fd015fa5e427ab8684e3f"
+PKG_VERSION="1.2.199"
+PKG_SHA256="cda034e5990aa92848bfd98045ae77000a789e37b2080a97d4cd7fbb3a089580"
 PKG_LICENSE="Apache 2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Tools"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-tools/archive/v${PKG_VERSION}.tar.gz"
@@ -14,10 +14,20 @@ pre_configure_target() {
   PKG_CMAKE_OPTS_TARGET="-DBUILD_ICD=Off \
                          -DINSTALL_ICD=Off"
 
-  # Conditionally disable Wayland support - doesn't accept CMake opts 
+  # Conditionally disable Wayland/X11 support - doesn't accept CMake opts 
   # https://github.com/KhronosGroup/Vulkan-Tools/issues/475
-  if [ ! "${DISPLAYSERVER}" = "weston" ]; then
+  if [ "${DISPLAYSERVER}" = "x11" ]; then
     sed -e "s/Build Wayland WSI support\" ON/Build Wayland WSI support\" OFF/" -i ${PKG_BUILD}/cube/CMakeLists.txt
+    sed -e "s/Build Wayland WSI support\" ON/Build Wayland WSI support\" OFF/" -i ${PKG_BUILD}/vulkaninfo/CMakeLists.txt
+  elif [ "${DISPLAYSERVER}" = "weston" ]; then
+    sed -e "s/Build XCB WSI support\" ON/Build XCB WSI support\" OFF/" -i   ${PKG_BUILD}/cube/CMakeLists.txt
+    sed -e "s/Build Xlib WSI support\" ON/Build Xlib WSI support\" OFF/" -i ${PKG_BUILD}/cube/CMakeLists.txt
+    sed -e "s/Build XCB WSI support\" ON/Build XCB WSI support\" OFF/" -i   ${PKG_BUILD}/vulkaninfo/CMakeLists.txt
+    sed -e "s/Build Xlib WSI support\" ON/Build Xlib WSI support\" OFF/" -i ${PKG_BUILD}/vulkaninfo/CMakeLists.txt
+  else
+    PKG_CMAKE_OPTS_TARGET+=" -DBUILD_CUBE=OFF"
+    sed -e "s/Build XCB WSI support\" ON/Build XCB WSI support\" OFF/" -i         ${PKG_BUILD}/vulkaninfo/CMakeLists.txt
+    sed -e "s/Build Xlib WSI support\" ON/Build Xlib WSI support\" OFF/" -i       ${PKG_BUILD}/vulkaninfo/CMakeLists.txt
     sed -e "s/Build Wayland WSI support\" ON/Build Wayland WSI support\" OFF/" -i ${PKG_BUILD}/vulkaninfo/CMakeLists.txt
   fi
 }
