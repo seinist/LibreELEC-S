f367a7411f199910f3eff5658806484c43f00211
SupervisedThinking <supervisedthinking@gmail.com>
2021-11-18 22:40:14 +0100

    vulkan: updates for gbm, wayland, x11

--- packages/supervisedthinking/graphics/vulkan/vulkan-loader/package.mk
+++ packages/supervisedthinking/graphics/vulkan/vulkan-loader/package.mk
@@ -1,34 +1,39 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+# Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="vulkan-loader"
 PKG_VERSION="1.2.199"
 PKG_SHA256="cc496f6725c7e088510d1a5e7c6a97b61e356b147dcc3d697233ca775dd768ef"
-PKG_LICENSE="Apache 2.0"
+PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Loader"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-Loader/archive/v${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain cmake:host vulkan-headers"
+PKG_DEPENDS_TARGET="toolchain vulkan-headers"
 PKG_LONGDESC="Vulkan Installable Client Driver (ICD) Loader."
 
 configure_package() {
   # Displayserver Support
   if [ "${DISPLAYSERVER}" = "x11" ]; then
-    PKG_DEPENDS_TARGET+=" libX11 libXrandr libxcb"
+    PKG_DEPENDS_TARGET+=" libxcb libX11"
+  elif [ "${DISPLAYSERVER}" = "weston" ]; then
+    PKG_DEPENDS_TARGET+=" wayland"
   fi
 }
 
 pre_configure_target() {
-  PKG_CMAKE_OPTS_TARGET="-DBUILD_TESTS=Off \
-                         -DGIT_FOUND=Off"
+  PKG_CMAKE_OPTS_TARGET="-DBUILD_TESTS=OFF"
 
   if [ "${DISPLAYSERVER}" = "x11" ]; then
-    PKG_CMAKE_OPTS_TARGET+=" -DBUILD_WSI_WAYLAND_SUPPORT=Off"
+    PKG_CMAKE_OPTS_TARGET+=" -DBUILD_WSI_XCB_SUPPORT=ON \
+                             -DBUILD_WSI_XLIB_SUPPORT=ON \
+                             -DBUILD_WSI_WAYLAND_SUPPORT=OFF"
   elif [ "${DISPLAYSERVER}" = "weston" ]; then
-    PKG_CMAKE_OPTS_TARGET+=" -DBUILD_WSI_XCB_SUPPORT=Off \
-                             -DBUILD_WSI_XLIB_SUPPORT=Off"
+    PKG_CMAKE_OPTS_TARGET+=" -DBUILD_WSI_XCB_SUPPORT=OFF \
+                             -DBUILD_WSI_XLIB_SUPPORT=OFF \
+                             -DBUILD_WSI_WAYLAND_SUPPORT=ON"
   else
-    PKG_CMAKE_OPTS_TARGET+=" -DBUILD_WSI_XCB_SUPPORT=Off \
-                             -DBUILD_WSI_XLIB_SUPPORT=Off \
-                             -DBUILD_WSI_WAYLAND_SUPPORT=Off"
+    PKG_CMAKE_OPTS_TARGET+=" -DBUILD_WSI_XCB_SUPPORT=OFF \
+                             -DBUILD_WSI_XLIB_SUPPORT=OFF \
+                             -DBUILD_WSI_WAYLAND_SUPPORT=OFF"
   fi
 }
