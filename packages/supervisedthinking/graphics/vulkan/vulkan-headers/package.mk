eed120e6b75de2cebbdad7fff22a82fa169daa1c
SupervisedThinking <supervisedthinking@gmail.com>
2021-11-18 22:40:14 +0100

    vulkan: updates for gbm, wayland, x11

--- packages/supervisedthinking/graphics/vulkan/vulkan-headers/package.mk
+++ packages/supervisedthinking/graphics/vulkan/vulkan-headers/package.mk
@@ -1,11 +1,12 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+# Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="vulkan-headers"
 PKG_VERSION="1.2.199"
 PKG_SHA256="c4b860e267312364aa0b36e457e5ac8c114fe0957f216668c9da9211ef4643d0"
-PKG_LICENSE="Apache 2.0"
+PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Headers"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-Headers/archive/v${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain cmake:host"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Vulkan Header files and API registry"
