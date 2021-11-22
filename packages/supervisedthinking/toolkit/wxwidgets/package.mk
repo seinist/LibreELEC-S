eaa05ad15342bab9b94f54fd19d6fcb697236d96
SupervisedThinking <supervisedthinking@gmail.com>
2021-11-22 17:08:58 +0100

    wxwidgets: updated to 3.1.5 / updated build opts for Wayland

--- packages/supervisedthinking/toolkit/wxwidgets/package.mk
+++ packages/supervisedthinking/toolkit/wxwidgets/package.mk
@@ -2,31 +2,25 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="wxwidgets"
-PKG_VERSION="3.0.5.1"
-PKG_SHA256="bae4d9f289e33a05fb8553fcc580564d30efe6a882ff08e3d4e09ef01f5f6578"
+PKG_VERSION="3.1.5"
+PKG_SHA256="e8fd5f9fbff864562aa4d9c094f898c97f5e1274c90f25beb0bfd5cb61319dea"
 PKG_LICENSE="wxWidgets licence"
 PKG_SITE="https://github.com/wxWidgets/wxWidgets"
 PKG_URL="https://github.com/wxWidgets/wxWidgets/archive/v${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain libSM libpng tiff mesa gtk3-system"
+PKG_DEPENDS_TARGET="toolchain libpng tiff mesa gtk3-system"
 PKG_LONGDESC="wxWidgets is a free and open source cross-platform C++ framework for writing advanced GUI applications using native controls."
-PKG_TOOLCHAIN="configure"
-PKG_DEPENDS_CONFIG="pango"
+
+configure_package() {
+  # Build with XCB support for X11
+  if [ ${DISPLAYSERVER} = "x11" ]; then
+    PKG_DEPENDS_TARGET+=" libSM"
+  elif [ ${DISPLAYSERVER} = "weston" ]; then
+    PKG_DEPENDS_TARGET+=" wayland"
+  fi
+}
 
 pre_configure_target() {
-  PKG_CONFIGURE_OPTS_TARGET="--disable-option-checking \
-                             --enable-unicode \
-                             --enable-mediactrl \
-                             --enable-webview \
-                             --enable-graphics_ctx \
-                             --disable-precomp-headers \
-                             --with-gtk=3 \
-                             --with-libpng=sys \
-                             --with-libjpeg=sys \
-                             --with-libtiff=sys \
-                             --with-libxpm=sys \
-                             --without-gtkprint \
-                             --with-opengl \
-                             --with-regex=builtin"
+  PKG_CMAKE_OPTS_TARGET="-Wno-dev"
 }
 
 post_makeinstall_target() {
@@ -39,8 +33,9 @@ post_makeinstall_target() {
     cp -PR lib/libwx_gtk3u_core-${PKG_VERSION:0:3}* ${INSTALL}/usr/lib
     cp -PR lib/libwx_gtk3u_adv-${PKG_VERSION:0:3}*  ${INSTALL}/usr/lib
 
-  # Fix wx-config paths                              
-  WX_CONFIG_PATH=${SYSROOT_PREFIX}/usr/lib/wx/config/${TARGET_NAME}-gtk3-unicode-${PKG_VERSION:0:3}
+  # Fix wx-config paths
+  WX_CONFIG_PATH=${SYSROOT_PREFIX}/usr/lib/wx/config/gtk3-unicode-${PKG_VERSION:0:3}
+    ln -sf ${WX_CONFIG_PATH} ${SYSROOT_PREFIX}/usr/bin/wx-config
     sed -e "s:^prefix=.*:prefix=${PKG_ORIG_SYSROOT_PREFIX}/usr:g"                         -i ${WX_CONFIG_PATH}
     sed -e "s:^wxconfdir=.*:wxconfdir=\"${PKG_ORIG_SYSROOT_PREFIX}/usr/lib/wx/config\":g" -i ${WX_CONFIG_PATH}
     sed -e "s:^libdir=.*:libdir=\"${PKG_ORIG_SYSROOT_PREFIX}/usr/lib\":g"                 -i ${WX_CONFIG_PATH}
