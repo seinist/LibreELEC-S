9d6a6218d5910229a21a1c252e39712697928013
SupervisedThinking <supervisedthinking@gmail.com>
2021-11-22 17:07:26 +0100

    libepoxy: updated to 1.5.9 / updated build opts for Wayland

--- packages/graphics/libepoxy/package.mk
+++ packages/graphics/libepoxy/package.mk
@@ -8,22 +8,29 @@
 # in Xorg.log
 
 PKG_NAME="libepoxy"
-PKG_VERSION="1.5.5"
-PKG_SHA256="5d80a43a6524a1ebdd0c9c5d5105295546a0794681853c636a0c70f8f9c658ce"
+PKG_VERSION="1.5.9"
+PKG_SHA256="ee8048d20179a2e86156ac842ddb6428732d9cd7a2cfc2eca905165bf24887a2"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/anholt/libepoxy"
 PKG_URL="https://github.com/anholt/libepoxy/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Epoxy is a library for handling OpenGL function pointer management for you."
 
-if [ "${OPENGL_SUPPORT}" = "yes" ]; then
-  PKG_DEPENDS_TARGET+=" ${OPENGL}"
-fi
+configure_package() {
+  if [ "${OPENGL_SUPPORT}" = "yes" ]; then
+    PKG_DEPENDS_TARGET+=" ${OPENGL}"
+  fi
 
-if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
-  PKG_DEPENDS_TARGET+=" ${OPENGLES}"
-fi
+  if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
+    PKG_DEPENDS_TARGET+=" ${OPENGLES}"
+  fi
+}
 
-if [ "${DISPLAYSERVER}" != "x11" ]; then
-  PKG_MESON_OPTS_TARGET="-Dglx=no"
-fi
+pre_configure_target() {
+  PKG_MESON_OPTS_TARGET="-Dtests=false"
+
+  if [ "${DISPLAYSERVER}" != "x11" ]; then
+    PKG_MESON_OPTS_TARGET+=" -Dglx=no \
+                            -Dx11=false"
+  fi
+}
