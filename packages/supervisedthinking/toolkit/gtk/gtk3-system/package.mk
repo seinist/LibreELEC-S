838cdfd2f197c1fd0dc696c4600a8f4b96c0f3a6
SupervisedThinking <supervisedthinking@gmail.com>
2021-11-22 17:08:40 +0100

    gtk3-system: updated build opts for Wayland

--- packages/supervisedthinking/toolkit/gtk/gtk3-system/package.mk
+++ packages/supervisedthinking/toolkit/gtk/gtk3-system/package.mk
@@ -7,24 +7,40 @@ PKG_SHA256="5b56fe97ea5c4c0541048aecdc21b2286941ca99ca035faca744875323a2699d"
 PKG_LICENSE="LGPL-2.0-or-later"
 PKG_SITE="http://www.gtk.org/"
 PKG_URL="https://github.com/GNOME/gtk/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain at-spi2-atk atk cairo gdk-pixbuf glib libX11 libXi libXrandr libepoxy pango"
-PKG_DEPENDS_CONFIG="libXft gdk-pixbuf"
+PKG_DEPENDS_TARGET="toolchain at-spi2-atk atk cairo gdk-pixbuf glib libepoxy pango"
+PKG_DEPENDS_CONFIG="gdk-pixbuf"
 PKG_LONGDESC="GTK is a multi-platform toolkit for creating graphical user interfaces."
 PKG_TOOLCHAIN="meson"
 
-PKG_MESON_OPTS_TARGET="-D x11_backend=true \
-                       -D wayland_backend=false \
-                       -D quartz_backend=false \
-                       -D xinerama=no \
-                       -D print_backends=file,lpr \
-                       -D colord=no \
-                       -D introspection=false \
-                       -D demos=false \
-                       -D examples=false \
-                       -D tests=false \
-                       -D builtin_immodules=yes"
+configure_package() {
+  # Build with XCB support for X11
+  if [ ${DISPLAYSERVER} = "x11" ]; then
+    PKG_DEPENDS_TARGET+=" libX11 libXi libXrandr"
+    PKG_DEPENDS_CONFIG+=" libXft"
+  elif [ ${DISPLAYSERVER} = "weston" ]; then
+    PKG_DEPENDS_TARGET+=" wayland"
+  fi
+}
 
 pre_configure_target() {
+  PKG_MESON_OPTS_TARGET="-D quartz_backend=false \
+                         -D xinerama=no \
+                         -D print_backends=file,lpr \
+                         -D colord=no \
+                         -D introspection=false \
+                         -D demos=false \
+                         -D examples=false \
+                         -D tests=false \
+                         -D builtin_immodules=yes"
+
+  if [ ${DISPLAYSERVER} = "x11" ]; then
+    PKG_MESON_OPTS_TARGET+=" -D x11_backend=true \
+                             -D wayland_backend=false"
+  elif [ ${DISPLAYSERVER} = "weston" ]; then
+    PKG_MESON_OPTS_TARGET+=" -D x11_backend=false \
+                             -D wayland_backend=true"
+  fi
+
   # ${TOOLCHAIN}/bin/glib-compile-resources requires ${TOOLCHAIN}/lib/libffi.so.6
   export LD_LIBRARY_PATH="${TOOLCHAIN}/lib:${LD_LIBRARY_PATH}"
   export GLIB_COMPILE_RESOURCES=glib-compile-resources GLIB_MKENUMS=glib-mkenums GLIB_GENMARSHAL=glib-genmarshal
