504db35f5011bdfd3288ce112fd8085556004725
SupervisedThinking <supervisedthinking@gmail.com>
2021-12-02 18:13:18 +0100

    vulkan: update to 1.2.201

--- packages/supervisedthinking/graphics/vulkan/vulkan-headers/package.mk
+++ packages/supervisedthinking/graphics/vulkan/vulkan-headers/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="vulkan-headers"
-PKG_VERSION="1.2.199"
-PKG_SHA256="c4b860e267312364aa0b36e457e5ac8c114fe0957f216668c9da9211ef4643d0"
+PKG_VERSION="1.2.201"
+PKG_SHA256="6b7f9c809acff4f0877e2e7722e02a08f2e17e06c6e2e8c84081631d15490009"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Headers"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-Headers/archive/v${PKG_VERSION}.tar.gz"
