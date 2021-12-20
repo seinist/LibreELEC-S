b7c133b5d1e055e255c0586d483e1581af68c6c7
SupervisedThinking <supervisedthinking@gmail.com>
2021-12-20 08:18:11 +0100

    moonlight-qt: updated to git e56bb67

--- packages/supervisedthinking/emulation/streaming/moonlight-qt/package.mk
+++ packages/supervisedthinking/emulation/streaming/moonlight-qt/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="moonlight-qt"
-PKG_VERSION="f0c292f50878d3fad12fe65b9d77b16063f39f8b" # v3.1.4+
+PKG_VERSION="e56bb6785f7a9e1dbbfad4764b0b631e64b0b471" # v3.1.4+
 PKG_LICENSE="GPL-3.0-or-later"
 PKG_SITE="https://github.com/moonlight-stream/moonlight-qt"
 PKG_URL="https://github.com/moonlight-stream/moonlight-qt.git"
