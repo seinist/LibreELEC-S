59a0161ec3a000ae8a66e9b0e40ac856a9843760
SupervisedThinking <supervisedthinking@gmail.com>
2021-12-26 11:56:40 +0100

    dosbox-staging: update to v0.78.0 - added libslirp dependency

--- packages/supervisedthinking/emulation/standalone/dosbox/dosbox-staging/package.mk
+++ packages/supervisedthinking/emulation/standalone/dosbox/dosbox-staging/package.mk
@@ -2,14 +2,14 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="dosbox-staging"
-PKG_VERSION="1826d7a0ae6ef3d3bf4bcf75aecf54fc6547bb86" #v0.77.1
+PKG_VERSION="33cca879170e8e5dcd295829f2fdb43c3f0ade89" #v0.78.0
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://dosbox-staging.github.io/"
 PKG_URL="https://github.com/dosbox-staging/dosbox-staging.git"
-PKG_DEPENDS_TARGET="toolchain linux alsa-lib sdl2 sdl2_net opusfile libpng fluidsynth-system munt"
+PKG_DEPENDS_TARGET="toolchain linux alsa-lib sdl2 sdl2_net opusfile libpng fluidsynth-system munt libslirp"
 PKG_LONGDESC="DOSBox Staging is an attempt to revitalize DOSBox's development process. It's not a rewrite, but a continuation and improvement on the existing DOSBox codebase while leveraging modern development tools and practices."
 GET_HANDLER_SUPPORT="git"
-PKG_GIT_CLONE_BRANCH="release/0.77.x"
+PKG_GIT_CLONE_BRANCH="release/0.78.x"
 PKG_GIT_CLONE_SINGLE="yes"
 PKG_BUILD_FLAGS="+lto +speed"
 
