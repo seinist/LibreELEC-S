7cee8cca5a6e82d09c54e86f00aabb5d28553585
SupervisedThinking <supervisedthinking@gmail.com>
2022-01-12 14:39:53 +0100

    sdl2_mixer: update to 2.0.4

--- packages/supervisedthinking/multimedia/SDL/sdl2_mixer/package.mk
+++ packages/supervisedthinking/multimedia/SDL/sdl2_mixer/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="sdl2_mixer"
-PKG_VERSION="12265697c94260d90e4247ccccfe25a92e1b4978"
-PKG_SHA256="c8ab04e13e9bffd5cd466b97ff5fbd223842d7723a64ddf5916f75271c8c863a"
+PKG_VERSION="2.0.4"
+PKG_SHA256="5605b6f230717acf6d09549671f7fe03f006700c61a61b86042888f81792e2b3"
 PKG_LICENSE="SDL"
 PKG_SITE="https://libsdl.org/projects/SDL_mixer/"
-PKG_URL="https://github.com/libsdl-org/SDL_mixer/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/libsdl-org/SDL_mixer/archive/release-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain sdl2 flac-system fluidsynth-system libvorbis-system mpg123-system opusfile"
 PKG_LONGDESC="An audio mixer that supports various file formats for Simple Directmedia Layer."
 PKG_TOOLCHAIN="configure"
