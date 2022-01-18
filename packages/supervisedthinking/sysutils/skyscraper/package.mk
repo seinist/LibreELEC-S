8411da9f6e7c66e0ace4861054a510bd0f7cb9ee
SupervisedThinking <supervisedthinking@gmail.com>
2022-01-18 01:09:40 +0100

    skyscraper: update to 3.7.0

--- packages/supervisedthinking/sysutils/skyscraper/package.mk
+++ packages/supervisedthinking/sysutils/skyscraper/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="skyscraper"
-PKG_VERSION="3.6.16"
-PKG_SHA256="11c833ec69d6d55c0438b83092f0077967d670ae19b2e0ceed815d4fa590b943"
+PKG_VERSION="3.7.0"
+PKG_SHA256="4a804a20f729e77f9b71c89776cdea99f93e2d5407d61519a8ab278e2c6e180c"
 PKG_LICENSE="GPL-3.0-or-later"
 PKG_SITE="https://github.com/muldjord/skyscraper"
 PKG_URL="https://github.com/muldjord/skyscraper/archive/${PKG_VERSION}.tar.gz"
