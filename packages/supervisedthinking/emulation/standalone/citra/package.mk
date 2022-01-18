4a05d88dde18b7b2dca8756d30d6b6b4a5e60b3d
SupervisedThinking <supervisedthinking@gmail.com>
2022-01-18 01:08:58 +0100

    citra: updated to git 45a4a56

--- packages/supervisedthinking/emulation/standalone/citra/package.mk
+++ packages/supervisedthinking/emulation/standalone/citra/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="citra"
-PKG_VERSION="0126ecb3e25295edc0768d20223d1358e330c622"
+PKG_VERSION="45a4a56264b3d26b4dd1a16b13feda4f9fd5fe37"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/citra-emu/citra"
