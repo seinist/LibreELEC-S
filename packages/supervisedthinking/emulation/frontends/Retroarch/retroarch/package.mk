4238c5218a892514451ef51c086ae36f9920e98d
SupervisedThinking <supervisedthinking@gmail.com>
2022-01-19 22:43:37 +0100

    retroarch: update to 1.10.0

--- packages/supervisedthinking/emulation/frontends/Retroarch/retroarch/package.mk
+++ packages/supervisedthinking/emulation/frontends/Retroarch/retroarch/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="retroarch"
-PKG_VERSION="06a23670081d46466198704ea5a3a8809a05291f" #v1.9.14
+PKG_VERSION="a14dcfa3bafd51548f0a005a7e83a646ccbd2f4d" #v1.10.0
 PKG_LICENSE="GPL-3.0-or-later"
 PKG_SITE="https://github.com/libretro/RetroArch"
 PKG_URL="https://github.com/libretro/RetroArch.git"
