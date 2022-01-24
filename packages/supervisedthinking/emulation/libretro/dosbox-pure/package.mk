48c3ff5e1b72bd0b8956e15fb2134c0f0ebd887a
SupervisedThinking <supervisedthinking@gmail.com>
2022-01-24 11:43:05 +0100

    dosbox-pure: update to 0.26

--- packages/supervisedthinking/emulation/libretro/dosbox-pure/package.mk
+++ packages/supervisedthinking/emulation/libretro/dosbox-pure/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="dosbox-pure"
-PKG_VERSION="0.25"
-PKG_SHA256="6d9b892124d9b63668c0932d4152a53af7bf11f82a4b651fe83015b4b17cf0d7"
+PKG_VERSION="0.26"
+PKG_SHA256="8d3201c1ede9bdc4059569171fa16f55f0b3cd27e61f78edfed5e68f9a0f1db8"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/schellingb/dosbox-pure"
 PKG_URL="https://github.com/schellingb/dosbox-pure/archive/${PKG_VERSION}.tar.gz"
