f18668a03dc3ebb67be597c225428d0156fbcf3d
SupervisedThinking <supervisedthinking@gmail.com>
2022-02-06 20:52:05 +0100

    libogg-system: build shared

--- packages/supervisedthinking/audio/libogg-system/package.mk
+++ packages/supervisedthinking/audio/libogg-system/package.mk
@@ -10,3 +10,6 @@ PKG_URL="http://downloads.xiph.org/releases/ogg/libogg-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Libogg contains necessary functionality to create, decode, and work with Ogg bitstreams."
 PKG_BUILD_FLAGS="+pic"
+
+PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=ON \
+                       -DINSTALL_DOCS=OFF"
