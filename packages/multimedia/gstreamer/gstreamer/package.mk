ff0caa5915ab2c0257c57a88919d039cd96e4fc3
SupervisedThinking <supervisedthinking@gmail.com>
2022-02-08 15:18:50 +0100

    gstreamer: fix missing libs

--- packages/multimedia/gstreamer/gstreamer/package.mk
+++ packages/multimedia/gstreamer/gstreamer/package.mk
@@ -11,36 +11,16 @@ PKG_DEPENDS_TARGET="toolchain ffmpeg libvorbis-system flac-system"
 PKG_LONGDESC="GStreamer open-source multimedia framework core library"
 
 pre_configure_target() {
-  PKG_MESON_OPTS_TARGET="-Dgst_debug=false \
-                         -Dgst_parse=true \
-                         -Dregistry=false \
-                         -Dtracer_hooks=false \
-                         -Doption-parsing=true \
-                         -Dpoisoning=false \
-                         -Dcheck=disabled \
-                         -Dlibunwind=disabled \
-                         -Dlibdw=disabled \
-                         -Ddbghelp=disabled \
-                         -Dbash-completion=disabled \
-                         -Dcoretracers=disabled \
-                         -Dexamples=disabled \
-                         -Dtests=disabled \
-                         -Dbenchmarks=disabled \
-                         -Dtools=disabled \
-                         -Ddoc=disabled \
-                         -Dintrospection=disabled \
-                         -Dnls=disabled \
-                         -Dgobject-cast-checks=disabled \
-                         -Dglib-asserts=disabled \
-                         -Dglib-checks=disabled \
-                         -Dextra-checks=disabled \
-                         -Dpackage-name="gstreamer"
-                         -Dpackage-origin="LibreELEC.tv"
-                         -Ddoc=disabled"
+PKG_MESON_OPTS_TARGET="-Dlibunwind=disabled \
+                       -Dexamples=disabled \
+                       -Dtests=disabled \
+                       -Dnls=disabled \
+                       -Dpackage-name=gstreamer \
+                       -Dpackage-origin=LibreELEC.tv \
+                       -Ddoc=disabled"
 }
 
 post_makeinstall_target() {
   # clean up
   safe_remove ${INSTALL}/usr/share
-  safe_remove ${INSTALL}/usr/lib/{libgstcontroller-1.0*,libgstnet-1.0*}
 }
