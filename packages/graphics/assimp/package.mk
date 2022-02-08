a28bb1c972a5f1a50b9ae15aa45606a5669246ae
SupervisedThinking <supervisedthinking@gmail.com>
2022-02-09 00:32:20 +0100

    assimp: initial package 5.2.1

---
+++ packages/graphics/assimp/package.mk
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="assimp"
+PKG_VERSION="5.2.1"
+PKG_SHA256="c9cbbc8589639cd8c13f65e94a90422a70454e8fa150cf899b6038ba86e9ecff"
+PKG_LICENSE="BSD"
+PKG_SITE="https://github.com/assimp/assimp"
+PKG_URL="https://github.com/assimp/assimp/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain zlib"
+PKG_LONGDESC="A library to import and export various 3d-model-formats including scene-post-processing to generate missing render data."
+
+PKG_CMAKE_OPTS_TARGET="-DASSIMP_BUILD_ASSIMP_TOOLS=OFF \
+                       -DASSIMP_BUILD_TESTS=OFF"
