d07131243b46e8d42a3ebc1611a285bdcca851a0
SupervisedThinking <supervisedthinking@gmail.com>
2022-03-30 12:53:43 +0200

    vulkan: update to 1.3.209

--- packages/graphics/vulkan/vulkan-tools/package.mk
+++ packages/graphics/vulkan/vulkan-tools/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="vulkan-tools"
-PKG_VERSION="1.3.208"
-PKG_SHA256="a09eb5b2b9c47830a10d38f062a3b737d3bfac72add9c5c2608f9ed0ffce316d"
+PKG_VERSION="1.3.209"
+PKG_SHA256="e7b8e4943b8040935365323362b1eefa8f4bd98b1517e92f86961b2a45ddbd60"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Tools"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-tools/archive/v${PKG_VERSION}.tar.gz"
