ac6a46ddc0efdbeee47c19a63855598c90adb335
SupervisedThinking <supervisedthinking@gmail.com>
2022-04-05 20:05:02 +0200

    dolphin-libretro: update to git 61475de & updated build

--- packages/supervisedthinking/emulation/libretro/dolphin-libretro/package.mk
+++ packages/supervisedthinking/emulation/libretro/dolphin-libretro/package.mk
@@ -2,23 +2,65 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="dolphin-libretro"
-PKG_VERSION="4ffaf058409d16e9fbdcf5a4061cc6ab61f1035f"
-PKG_SHA256="45621692537c284eb7ea81de1899c3362d500ff85b2b6ee1923b4a42e26850e4"
-PKG_ARCH="x86_64"
+PKG_VERSION="61475de768fb7457128853656e53a942aedd79fe"
+PKG_SHA256="6756ab315edc2fa02fb66667e36b2aac7fef8872215e1fb4a35f89e5fff9d4aa"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/libretro/dolphin"
 PKG_URL="https://github.com/libretro/dolphin/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain linux glibc systemd mesa xorg-server enet-system bluez lzo alsa-lib ffmpeg curl libpng zlib"
+PKG_DEPENDS_TARGET="toolchain linux glibc systemd mesa enet-system bluez lzo alsa-lib ffmpeg curl libpng zlib zstd"
 PKG_LONGDESC="Dolphin is a GameCube / Wii emulator, allowing you to play games for these two platforms on PC with improvements."
 PKG_BUILD_FLAGS="+lto -sysroot"
 
 PKG_LIBNAME="dolphin_libretro.so"
 PKG_LIBPATH="${PKG_LIBNAME}"
 
+configure_package() {
+  # Displayserver Support
+  if [ "${DISPLAYSERVER}" = "x11" ]; then
+    PKG_DEPENDS_TARGET+=" xorg-server"
+  fi
+
+  # OpenGL Support
+  if [ "${OPENGL_SUPPORT}" = "yes" ]; then
+    PKG_DEPENDS_TARGET+=" ${OPENGL}"
+  fi
+
+  # OpenGLES Support
+  if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
+    PKG_DEPENDS_TARGET+=" ${OPENGLES}"
+  fi
+
+  # Vulkan Support
+  if [ "${VULKAN_SUPPORT}" = "yes" ]; then
+    PKG_DEPENDS_TARGET+=" ${VULKAN}"
+  fi
+}
+
 pre_configure_target() {
-  PKG_CMAKE_OPTS_TARGET="-D ENABLE_QT=OFF \
+  PKG_CMAKE_OPTS_TARGET="-DLIBRETRO=ON \
+                         -DUSE_OPENMP=OFF"
+
+  if [ "$OPENGLES_SUPPORT" = yes ]; then
+    PKG_CMAKE_OPTS_TARGET+=" -DUSE_GLES=ON"
+  fi
+
+  if [ "$VULKAN_SUPPORT" = yes ]; then
+    PKG_CMAKE_OPTS_TARGET+=" -DUSE_VULKAN=ON"
+  fi
+}
+
+pre_configure_target() {
+  PKG_CMAKE_OPTS_TARGET="-D USE_SHARED_ENET=on \
+                         -D ENABLE_NOGUI=OFF \
+                         -D ENABLE_QT=OFF \
                          -D ENABLE_LTO=OFF \
+                         -D USE_DISCORD_PRESENCE=OFF \
+                         -D ENABLE_TESTS=OFF \
                          -D LIBRETRO=ON"
+
+  if [ "${DISPLAYSERVER}" != "x11" ]; then
+    PKG_CMAKE_OPTS_TARGET+=" -D ENABLE_X11=OFF"
+  fi
 }
 
 pre_make_target() {
