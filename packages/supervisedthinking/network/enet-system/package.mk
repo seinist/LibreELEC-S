202460b103f17c6db1553ac87c46e4817559bb1f
SupervisedThinking <supervisedthinking@gmail.com>
2022-04-05 19:59:40 +0200

    enet-system: update to 1.3.17 & build shared

--- packages/supervisedthinking/network/enet-system/package.mk
+++ packages/supervisedthinking/network/enet-system/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="enet-system"
-PKG_VERSION="1.3.16"
-PKG_SHA256="b3aa85b43e4309fec9441b4e6639c268e22962a578bd5e2307bb3a7b6fe73714"
+PKG_VERSION="1.3.17"
+PKG_SHA256="1e0b4bc0b7127a2d779dd7928f0b31830f5b3dcb7ec9588c5de70033e8d2434a"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/lsalzman/enet/"
 PKG_URL="https://github.com/lsalzman/enet/archive/v${PKG_VERSION}.tar.gz"
@@ -12,7 +12,7 @@ PKG_LONGDESC="A network communication layer on top of UDP (User Datagram Protoco
 PKG_TOOLCHAIN="autotools"
 PKG_BUILD_FLAGS="+pic"
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared"
+PKG_CONFIGURE_OPTS_TARGET="--disable-static"
 
 post_makeinstall_target() {
   safe_remove ${INSTALL}
