ec288efc40bde0f64309a2ecef5a45bb591e981a
SupervisedThinking <supervisedthinking@gmail.com>
2022-04-14 00:51:41 +0200

    stella2014: update to git 1351a4f

--- packages/supervisedthinking/emulation/libretro/stella2014/package.mk
+++ packages/supervisedthinking/emulation/libretro/stella2014/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="stella2014"
-PKG_VERSION="934c7a2a44ef038af529b68950ddba4f7ea3478e"
-PKG_SHA256="8a4d10ce4646f0d474efc339e4db9211dd36bbf3089e98f236367368b231a8d9"
+PKG_VERSION="1351a4fe2ca6b1f3a66c7db0df2ec268ab002d41"
+PKG_SHA256="7836f41bf95a345735701e818e340ffcfa008542bb5ad0b2f760d807bd3b3620"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/libretro/stella2014-libretro"
 PKG_URL="https://github.com/libretro/stella2014-libretro/archive/${PKG_VERSION}.tar.gz"
