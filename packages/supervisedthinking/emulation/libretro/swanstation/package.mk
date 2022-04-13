eb6f3b99083114cf715cc4b771dfee4a8ebcac01
SupervisedThinking <supervisedthinking@gmail.com>
2022-04-14 00:51:59 +0200

    swanstation: update to git 0e27584

--- packages/supervisedthinking/emulation/libretro/swanstation/package.mk
+++ packages/supervisedthinking/emulation/libretro/swanstation/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="swanstation"
-PKG_VERSION="6794d8909bd19ae3cedf5740884f41d5fc33352b"
-PKG_SHA256="08d66d1aa51a84f83cb3eb8a0cea5c06685ecaf17b2d61e4064c91ac54987814"
+PKG_VERSION="0e27584155a9ecbd63111afb9c8646148aaba46b"
+PKG_SHA256="ded4e445e5670d48379436c896c45cd6465c90094901393e5805049deffee0c1"
 PKG_LICENSE="GPL-3.0-or-later"
 PKG_SITE="https://github.com/libretro/swanstation"
 PKG_URL="https://github.com/libretro/swanstation/archive/${PKG_VERSION}.tar.gz"
