726285490fe1f27be37b83a2a0f8e54d17281669
SupervisedThinking <supervisedthinking@gmail.com>
2022-04-14 00:39:57 +0200

    atari800: update to git beab30e

--- packages/supervisedthinking/emulation/libretro/atari800/package.mk
+++ packages/supervisedthinking/emulation/libretro/atari800/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="atari800"
-PKG_VERSION="478a8ec99a7f8436a39d5ac193c5fe313233ee7b"
-PKG_SHA256="10a13295036cdc27d28852e348227c874f5c0f14bc41b9c4f2fc3e4473a66c6c"
+PKG_VERSION="beab30e7ea10b7ed14d0514064f47d16f76cd995"
+PKG_SHA256="98e6ba7845dfa8295d9d93278b8c469ff91c445e4790d268473a0820be456572"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/libretro/libretro-atari800"
 PKG_URL="https://github.com/libretro/libretro-atari800/archive/${PKG_VERSION}.tar.gz"
