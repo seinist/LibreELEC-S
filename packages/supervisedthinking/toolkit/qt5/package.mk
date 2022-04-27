399856198ba799629661243b09c4822af949ff7b
SupervisedThinking <supervisedthinking@gmail.com>
2022-04-27 18:46:42 +0200

    qt5: update to git 016891a 5.15.3+ (KDE Qt5PatchCollection)

--- packages/supervisedthinking/toolkit/qt5/package.mk
+++ packages/supervisedthinking/toolkit/qt5/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking @ gmail.com)
 
 PKG_NAME="qt5"
-PKG_VERSION="58c28d42da6fdef1a4c8957703a5f34668c169d0" # 5.15.3+ (KDE Qt5PatchCollection)
+PKG_VERSION="016891a2a4e04ce4d11f743db2f3cfa45dd73df6" # 5.15.3+ (KDE Qt5PatchCollection)
 PKG_LICENSE="GPL"
 PKG_SITE="http://qt-project.org"
 PKG_URL="https://invent.kde.org/qt/qt/qt5.git"
