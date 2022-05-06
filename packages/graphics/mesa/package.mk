96bf021fdc3051a29535c9c383df1ba5c4879f04
SupervisedThinking <supervisedthinking@gmail.com>
2022-05-06 10:50:35 +0200

    mesa: update to 22.1.0-rc4

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="22.1.0-rc3"
-PKG_SHA256="728f26a02c533d5787865dca1ce6f31885aa46463d2d39a2d14722980d9ff69c"
+PKG_VERSION="22.1.0-rc4"
+PKG_SHA256="f913712539f823e133c2b583382aa1626156dd858e68c38f58d0fcac85e37a44"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://mesa.freedesktop.org/archive/mesa-${PKG_VERSION}.tar.xz"
