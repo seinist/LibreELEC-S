7a84acc48517fd8a55ff260ebd5e389b165e620c
SupervisedThinking <supervisedthinking@gmail.com>
2022-05-11 20:41:00 +0200

    pcsx2: update to r1.7.2736

--- packages/supervisedthinking/emulation/standalone/pcsx2/package.mk
+++ packages/supervisedthinking/emulation/standalone/pcsx2/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="pcsx2"
-PKG_VERSION="eefc81ed8c03389cc9630398a54c32576f15beae" #v1.7.2705
+PKG_VERSION="0bb7cc0a1ade98019c0e921ff88f02670af8c60b" #v1.7.2736
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/PCSX2/pcsx2"
