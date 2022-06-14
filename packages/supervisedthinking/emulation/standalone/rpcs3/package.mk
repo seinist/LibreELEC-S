750e685bf230b56a7b4460034f8f5ff65b144c6e
SupervisedThinking <supervisedthinking@gmail.com>
2022-06-14 19:04:11 +0200

    rpcs3: update to r0.0.22-13785

--- packages/supervisedthinking/emulation/standalone/rpcs3/package.mk
+++ packages/supervisedthinking/emulation/standalone/rpcs3/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="rpcs3"
-PKG_VERSION="7530b3c971fed8025f40ac4f920520995304d0c5" # 0.0.22-13764
+PKG_VERSION="c2190f71caf788edd714e057f89d9b76cce5de02" # 0.0.22-13785
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://rpcs3.net"
